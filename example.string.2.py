9f13079a6fd0705fbd780f8d1b23b622fa670176
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-29 18:52:39 +0000

    Add files via upload

---
+++ example.string.2.py
@@ -0,0 +1,25 @@
+my= "hello %s how can i help you" %"zino"
+my_number="my nymber %s is " % 5
+my_num ="this is %d the right number" % 98
+my__num ="the wrong number is %.2f " % 0.21
+my1 ="num1= %s, num2= %s,num3= %s" %(11,22,33)
+myd="the caracter is %c " % "A"
+x="%x" %65
+y="%o" %65
+mystr="HEllo"
+mystrr=mystr.upper()
+mystra =mystr.lower()
+
+
+print(my)
+print(my_number)
+print(my_num)
+print(my__num)
+print(my1)
+print(myd)
+print(x)
+print(y)
+print(mystrr)
+print(mystra)
+print(mystr.isupper())
+print(mystr.islower())
